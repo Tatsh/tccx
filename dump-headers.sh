f88bc7f321e62d425895741aaa2df64a3244d9c3
Andrew Udvare <audvare@gmail.com>
2026-07-08 11:52:01 +0000

    headers: drop empty/boilerplate class-dump output (libTCCD, TCCDDatabase, NSObject protocol); prune in dump-headers.sh

--- dump-headers.sh
+++ dump-headers.sh
@@ -6,3 +6,6 @@ class-dump \
 sed -r -e '/^\-\s+\(void\)\.cxx_destruct;$/d' -e 's:;\s+// @synthesize.*:;:g' \
     -i headers/*.h
 clang-format -i headers/*.h
+# Drop class-dump noise carrying no TCC-specific information: empty interfaces
+# (libTCCD, TCCDDatabase) and the verbatim system NSObject protocol.
+rm -f headers/libTCCD.h headers/TCCDDatabase.h headers/NSObject-Protocol.h
